5a99b3e4d72ff035828cba54ea4070242b0d1f20
Oskar Młynarczuk <48478429+Cecr2504a@users.noreply.github.com>
2019-10-16 14:26:28 +0200

    Add files via upload

---
+++ RaW_pd1/zajecia3_stala_dalmierza.m
@@ -0,0 +1,21 @@
+clear
+clc
+load stala_dodawania.mat
+format longg
+Dp=stala_dodawania(:,1); %wart prawdziwa
+Dm=stala_dodawania(:,2); % wart pomierzona
+%Dp=Dm(1+s)+k - wzór
+A=[Dm ones(size(Dm))];
+k=Dm-Dp;
+L=-k;
+X=-inv(A'*A)*(A'*L)
+%K=sum(k)/length(k)
+%v=K-k
+%z=v*v
+%m=sqrt(sum(z)/(length(k)-1))
+V=A*X+L
+%s=(Dp-K)./Dm-1
+%S=sum(s)./length(s)
+T=A'*V
+R=V'*V
+U=V'*L
