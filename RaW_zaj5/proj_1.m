84738d88bb5fe1b9ccf7e866d7cc4e781522b84a
Oskar Młynarczuk <48478429+Cecr2504a@users.noreply.github.com>
2019-10-30 14:12:10 +0100

    Add files via upload

---
+++ RaW_zaj5/proj_1.m
@@ -0,0 +1,30 @@
+clear
+clc
+Hr=1.00
+h1ob=1.04
+h2ob=2.05
+h3ob=3.03
+A=[1 0; -1 1;0 1]
+z=size(A)(1)-size(A)(2)
+L=[-Hr-h1ob;-h2ob;-Hr-h3ob]
+% wagi
+m1=0.02
+m2=0.02
+m3=0.04
+m=[1/m1^2,1/m2^2,1/m3^2]
+P=diag(m)
+X=-inv(A'*P*A)*(A'*P*L) %estymator wektora niewiadomych
+% poprawka
+V=A*X+L
+% Kontrole
+kont=A'*P*V
+% tu mo¿na zrobiæ tak abs(VTPV-(LTPAX+LTPL))<0.06
+t=V'*P*V
+o=V'*P*L
+sigma=sqrt(V'*P*V/z) %sigma>1, wagi za du¿e sigma<1, wagi za ma³e
+% H wyrównane hwyr=hob+v
+h1wyr=h1ob+V(1)
+h2wyr=h2ob+V(2)
+h3wyr=h3ob+V(3)
+% 3 test hwyrb-hwyra=H3 itp
+
---
+++ RaW_zaj5/zad_4_15.m
@@ -0,0 +1,31 @@
+clear
+clc
+%Dane
+% H reperów 1 i 2
+Hr=[100.127;115.430];
+% h obserwowane punktów 1,2,3,4,5
+hob=[2.370;4.718;-8.224;2.909;-1.810]
+A=[1 0 0; -1 1 0;0 1 0; 0 1 -1; 1 0 -1]
+z=size(A)(1)-size(A)(2);
+L=[-Hr(1)-hob(1);-hob(2);-Hr(2)-hob(3);-hob(4); -hob(5)]
+% wagi punktów 1,2,3,4,5
+m=[4,4,2,2,4]*0.001;
+P=inv(diag(m.^2))
+X=-inv(A'*P*A)*(A'*P*L) %estymator wektora niewiadomych
+% poprawka
+V=A*X+L
+% Kontrole
+kont=A'*P*V
+t=V'*P*V
+o=V'*P*L
+sigma=sqrt(V'*P*V/z) %sigma>1, wagi za du¿e sigma<1, wagi za ma³e
+% H wyrównane hwyr=hob+v
+hwyr=hob+V
+% kontrola 
+sumhwyr=sum(hwyr)
+h1wyr=X(1)-Hr(1);
+h2wyr=X(2)-X(1);
+h3wyr=X(2)-Hr(2);
+h4wyr=X(2)-X(3);
+h5wyr=X(1)-X(3);
+hwyrk=[h1wyr;h2wyr;h3wyr;h4wyr;h5wyr]
