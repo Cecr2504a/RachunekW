93de412fd133baee23a3ee18f7e334373a8d9bb4
Oskar Młynarczuk <48478429+Cecr2504a@users.noreply.github.com>
2019-10-30 14:22:11 +0100

    Add files via upload

---
+++ RaW_zaj4/proj_1.m
@@ -0,0 +1,23 @@
+clear
+clc
+Hr=1.00
+h1ob=1.04
+h2ob=2.05
+h3ob=3.03
+A=[1 0; -1 1;0 1]
+z=size(A)(1)-size(A)(2)
+L=[-Hr-h1ob;-h2ob;-Hr-h3ob]
+m1=0.02
+m2=0.02
+m3=0.04
+m=[1/m1^2,1/m2^2,1/m3^2]
+P=diag(m)
+X=-inv(A'*P*A)*(A'*P*L)
+V=A*X+L
+kont=A'*P*V
+t=V'*P*V
+o=V'*P*L
+sigma=sqrt(V'*P*V/z)
+h1wyr=h1ob+V(1)
+h2wyr=h2ob+V(2)
+h3wyr=h3ob+V(3)
---
+++ RaW_zaj4/zad_4_15.m
@@ -0,0 +1,23 @@
+clear
+clc
+Hr=[100.127;115.430];
+hob=[2.370;4.718;-8.224;2.909;-1.810]
+A=[1 0 0; -1 1 0;0 1 0; 0 1 -1; 1 0 -1]
+z=size(A)(1)-size(A)(2);
+L=[-Hr(1)-hob(1);-hob(2);-Hr(2)-hob(3);-hob(4); -hob(5)]
+m=[4,4,2,2,4]*0.001;
+P=inv(diag(m.^2))
+X=-inv(A'*P*A)*(A'*P*L)
+V=A*X+L
+kont=A'*P*V
+t=V'*P*V
+o=V'*P*L
+sigma=sqrt(V'*P*V/z)
+hwyr=hob+V
+sumhwyr=sum(hwyr)
+h1wyr=X(1)-Hr(1);
+h2wyr=X(2)-X(1);
+h3wyr=X(2)-Hr(2);
+h4wyr=X(2)-X(3);
+h5wyr=X(1)-X(3);
+hwyrk=[h1wyr;h2wyr;h3wyr;h4wyr;h5wyr]
