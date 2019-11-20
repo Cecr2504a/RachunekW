d8b4d00d6682ec2097b916a24bd86d66674c1815
Oskar Młynarczuk <48478429+Cecr2504a@users.noreply.github.com>
2019-11-20 16:43:21 +0100

    Update TEST.m

--- RaW_zaj7/TEST.m
+++ RaW_zaj7/TEST.m
@@ -3,6 +3,7 @@ b=strrep(a,',','.')
 c=cell2mat(cellfun(@str2num,b,'un',0))
 [po,bo]=size(c);
 A=zeros(po,1);
+Lr=zeros(po,1);
 n=0;
 while n<po;
    n=n+1;
@@ -10,9 +11,17 @@ while n<po;
      kol=c(n,1);
      A(n,kol)=-1;
    end
+   if c(n,1)<100
+     rep=c(n,1)-100
+     Lr(n,1)=-Hr(rep)
+   end
    if c(n,2)<100;
      kol2=c(n,2);
      A(n,kol2)=1;
+   end
+   if c(n,2)<100
+     rep=c(n,2)-100
+     Lr(n,1)=Hr(rep)
    end
 end
 A
