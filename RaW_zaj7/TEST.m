626efc7deaed61418cd54bda5d227ae217ad62d3
Oskar Młynarczuk <48478429+Cecr2504a@users.noreply.github.com>
2019-11-20 16:28:01 +0100

    Update TEST.m

--- RaW_zaj7/TEST.m
+++ RaW_zaj7/TEST.m
@@ -1,23 +1,20 @@
 a=importdata('Test.txt')
 b=strrep(a,',','.')
 c=cell2mat(cellfun(@str2num,b,'un',0))
-[po,bo]=size(c)
-A=[]
-n=0
-while n<po
-   n=n+1
-   if c(n,1)<100
-     kol=c(n,1)
-     A(n,kol)=-1
+[po,bo]=size(c);
+A=zeros(po,1);
+n=0;
+while n<po;
+   n=n+1;
+   if c(n,1)<100;
+     kol=c(n,1);
+     A(n,kol)=-1;
    end
-   if c(n,1)>100
-     wyn(n,1)=0
-   end
-   if c(n,2)<100
-     kol2=c(n,2)
-     wyn(kol2,2)=1
-   end
-   if c(n,2)>100
-     wyn(n,2)=0
+   if c(n,2)<100;
+     kol2=c(n,2);
+     A(n,kol2)=1;
    end
 end
+A
+h=c(:,3)
+m=c(:,4)
