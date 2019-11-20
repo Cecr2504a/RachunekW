2be38029fa7721272680f7f882712354d1e5e868
Oskar Młynarczuk <48478429+Cecr2504a@users.noreply.github.com>
2019-11-20 16:47:20 +0100

    Update TEST.m

--- RaW_zaj7/TEST.m
+++ RaW_zaj7/TEST.m
@@ -11,19 +11,20 @@ while n<po;
      kol=c(n,1);
      A(n,kol)=-1;
    end
-   if c(n,1)<100
-     rep=c(n,1)-100
-     Lr(n,1)=-Hr(rep)
+   if c(n,1)<100;
+     rep=c(n,1)-100;
+     Lr(n,1)=-Hr(rep);
    end
    if c(n,2)<100;
      kol2=c(n,2);
      A(n,kol2)=1;
    end
-   if c(n,2)<100
-     rep=c(n,2)-100
-     Lr(n,1)=Hr(rep)
+   if c(n,2)<100;
+     rep=c(n,2)-100;
+     Lr(n,1)=Hr(rep);
    end
 end
 A
 h=c(:,3)
 m=c(:,4)
+L=h+Lr
